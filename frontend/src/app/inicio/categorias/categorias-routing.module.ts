0e75638cb25f79f6b29f39e32a4738c44d5bd61b
matiasarriagadalov <matiasarriagadalovino@gmail.com>
2020-11-09 18:55:20 -0300

    Creacion pagina Categorias

---
+++ frontend/src/app/inicio/categorias/categorias-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CategoriasPage } from './categorias.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: CategoriasPage
+  },
+  {
+    path: 'categoria',
+    loadChildren: () => import('./categoria/categoria.module').then( m => m.CategoriaPageModule)
+  },
+  {
+    path: 'productos',
+    loadChildren: () => import('./productos/productos.module').then( m => m.ProductosPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CategoriasPageRoutingModule {}
