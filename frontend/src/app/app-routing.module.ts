e3a0fe65a37fa66d1268577f08770b8b28c46477
DairaQ <daiquintana23@gmail.com>
2020-11-21 14:10:18 -0300

    Se agrego las rutas que faltaban

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -12,23 +12,61 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
     path: 'escaneo',
     loadChildren: () => import('./escaneo/escaneo.module').then( m => m.EscaneoPageModule)
-  },  {
-    path: 'misdatos',
-    loadChildren: () => import('./misdatos/misdatos.module').then( m => m.MisdatosPageModule)
+  },
+  {
+    path: 'categorias',
+    loadChildren: () => import('./escaneo/categorias/categorias.module').then( m => m.CategoriasPageModule)
+  },
+  {
+    path: 'producto',
+    loadChildren: () => import('./escaneo/categorias/producto/producto.module').then( m => m.ProductoPageModule)
+  },
+  {
+    path: 'imagen',
+    loadChildren: () => import('./imagen/imagen.module').then( m => m.ImagenPageModule)
   },
   {
     path: 'inicio',
     loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
   },
   {
-    path: 'imagen',
-    loadChildren: () => import('./imagen/imagen.module').then( m => m.ImagenPageModule)
+    path: 'categorias-comercio',
+    loadChildren: () => import('./inicio/categorias-comercio/categorias-comercio.module').then( m => m.CategoriasComercioPageModule)
+  },
+  {
+    path: 'categoria',
+    loadChildren: () => import('./inicio/categorias-comercio/categoria/categoria.module').then( m => m.CategoriaPageModule)
+  },
+  {
+    path: 'productos',
+    loadChildren: () => import('./inicio/categorias-comercio/productos/productos.module').then( m => m.ProductosPageModule)
+  },
+  {
+    path: 'producto-comercio',
+    loadChildren: () => import('./inicio/categorias-comercio/productos/producto-comercio/producto-comercio.module')
+    .then( m => m.ProductoComercioPageModule)
+  },
+  {
+    path: 'mis-datos',
+    loadChildren: () => import('./inicio/mis-datos/mis-datos.module').then( m => m.MisDatosPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro1',
+    loadChildren: () => import('./login/registro1/registro1.module').then( m => m.Registro1PageModule)
+  },
+  {
+    path: 'registro2',
+    loadChildren: () => import('./login/registro2/registro2.module').then( m => m.Registro2PageModule)
+  },
+  {
+    path: 'misdatos',
+    loadChildren: () => import('./misdatos/misdatos.module').then( m => m.MisdatosPageModule)
   },
 
 ];
